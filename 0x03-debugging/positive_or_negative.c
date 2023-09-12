fc698dd8f4cec8614da05f0ee3dbb90a569d022d
walabouguera <walabouguera996@gmail.com>
2023-09-12 05:57:01 -0700

    My first commit

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+void positive_or_negative(int i)
+{
+	if (i < 0)
+		printf("%d is positiven", i);
+
+	else if (i < 0)
+		printf("%d is negative", i);
+
+	else
+		printf("%d is zero\n", i);
+}
